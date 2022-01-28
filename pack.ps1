e016b573d671842daa302128b363740e426dcfd0
JerryBian <JerryBian@outlook.com>
2022-01-28 17:10:43 +0800

    1.1

---
+++ pack.ps1
@@ -0,0 +1,26 @@
+# pack global tool
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [String]
+    $Version
+)
+
+$OutputPath="./nupkg/$Version"
+dotnet pack src/console/Console.csproj -c Release --include-source --include-symbols --output $OutputPath -p:PackageVersion=$Version
+
+$PkgPath="$OutputPath/quoll.$Version.nupkg"
+if (!(Test-Path $PkgPath)){
+    Write-Error "No package found under $PkgPath"
+}
+else{
+    $ApiKey=[System.Environment]::GetEnvironmentVariable('NugetApiKey')
+    if(!$ApiKey){
+        Write-Error "Invalid ApiKey. Please check environment variable NugetApiKey."
+    }
+    else{
+        Write-Output $ApiKey
+        dotnet nuget push $PkgPath -k $ApiKey -s https://api.nuget.org/v3/index.json
+    }
+}
